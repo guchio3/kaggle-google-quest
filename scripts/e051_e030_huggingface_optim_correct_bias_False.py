bf487152f7111c0876ccd7c4dbec3782da6dbb7c
guchio3 <astt.hwhw@gmail.com>
2020-01-31 13:21:57 +0900

    temp

--- scripts/e051_e030_huggingface_optim_correct_bias_False.py
+++ scripts/e051_e030_huggingface_optim_correct_bias_False.py
@@ -591,7 +591,7 @@ def main(args, logger):
                                                            trn_dataset.tokenizer),
                                                        MAX_SEQUENCE_LENGTH=max_seq_len,
                                                        )
-        optimizer = AdamW(model.parameters(), lr=3e-5, correct_bias=False)
+        optimizer = AdamW(model.parameters(), lr=3e-5, correct_bias=False, eps=1e-7)
         scheduler = optim.lr_scheduler.CosineAnnealingLR(
             optimizer, T_max=MAX_EPOCH, eta_min=1e-5)
 
